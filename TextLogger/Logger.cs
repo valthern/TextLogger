7a31561d925fa649dd517fc98ff52d20e5324df6
Omar I. Martínez Gómez <omar_238@hotmail.com>
2020-03-23 17:55:15 -0600

    correcciones menores

--- TextLogger/Logger.cs
+++ TextLogger/Logger.cs
@@ -12,29 +12,23 @@ namespace TextLogger
         /// </summary>
         /// <param name="mensaje">Mensaje para registrar en el log.</param>
         /// <param name="append">"True" indica que se anexará al archivo existente esta información, "False" indica que creará uno nuevo sólo con esta información (sobrescribirá el archivo existente).</param>
-        /// <param name="prog">Nombre del programa que utiliza el Logger</param>
+        /// <param name="nombrePrograma">Nombre del programa que utiliza el Logger</param>
         public static void Logg(string mensaje, bool append, string nombrePrograma = "")
         {
             DateTime ahorita = DateTime.Now;
             string ruta;
 
-            string dir = Environment.GetEnvironmentVariable("SystemDrive") + @"\_Logs";
+            string directorio = Environment.GetEnvironmentVariable("SystemDrive") + @"\_Logs";
             string archivo = "Logs_" + nombrePrograma + ".txt";
             
-            if (string.IsNullOrEmpty(dir))
-            {
-                ruta = Path.Combine(dir, archivo);
-            }
+            if (string.IsNullOrWhiteSpace(directorio))
+                ruta = Path.Combine(directorio, archivo);
             else
-            {
                 ruta = Path.Combine(@"C:\_Logs", archivo);
-            }
 
-            if (!Directory.Exists(dir))
-            {
-                Directory.CreateDirectory(dir);
-            }
-
+            if (!Directory.Exists(directorio))
+                Directory.CreateDirectory(directorio);
+            
             using (StreamWriter sw = new StreamWriter(ruta, append))
             {
                 sw.WriteLine(" # [" + ahorita.ToString("yyyy-MM-dd HH:mm:ss") + "] => " + mensaje);
@@ -45,7 +39,7 @@ namespace TextLogger
         /// Método que registra un evento. Por Default se crea un directorio en la carpeta raíz del sistema, el cual se llamará _Logs. Dentro de él se creará un archivo con nombre Logs.txt para registrar los eventos anteriormente mencionados. Este método siempre adjuntará al archivo del log si es que existe.
         /// </summary>
         /// <param name="mensaje">Mensaje para registrar en el log.</param>
-        /// <param name="prog">Nombre del programa que utiliza el Logger</param>
+        /// <param name="nombrePrograma">Nombre del programa que utiliza el Logger</param>
         public static void Logg(string mensaje, string nombrePrograma = "")
         {
             Logg(mensaje, true, nombrePrograma);
